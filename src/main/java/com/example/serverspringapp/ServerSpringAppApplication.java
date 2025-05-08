29a05bd5c5577bb8b3d0ba504d69bcaf20efa212
mataa019 <johnnymataa019@gmail.com>
2025-05-08 16:29:51 +0200

    implementing the postgresql

---
+++ src/main/java/com/example/serverspringapp/ServerSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.serverspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerSpringAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerSpringAppApplication.class, args);
+    }
+
+}
