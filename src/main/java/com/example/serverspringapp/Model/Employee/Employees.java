e2f5649d6b25580dc3c7c906ea484c46305d7b48
mataa019 <johnnymataa019@gmail.com>
2025-05-08 16:36:31 +0200

    implementing the Employee Model

--- src/main/java/com/example/serverspringapp/Model/Employee/Employees.java
+++ src/main/java/com/example/serverspringapp/Model/Employee/Employees.java
@@ -11,4 +11,36 @@ public class Employees {
     private String name;
     private String location;
     private  String branch;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
 }
