2f1e8e12ca564a062f39810023beac9d45c6c200
mataa019 <johnnymataa019@gmail.com>
2025-05-09 16:59:57 +0200

    implementing mysql database

--- src/main/java/com/example/serverspringapp/EmployeeRepository.java
+++ src/main/java/com/example/serverspringapp/EmployeeRepository.java
@@ -1,7 +1,9 @@
 package com.example.serverspringapp;
 import com.example.serverspringapp.Model.Employee.Employees;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends CrudRepository<Employees, Long> {
 
 }
---
+++ src/main/java/com/example/serverspringapp/EmployeeService.java
@@ -0,0 +1,10 @@
+package com.example.serverspringapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+    @Autowired
+    private EmployeeRepository repository;
+}
