5cd7dc8f66147ad251f13b55a19daeee75511957
mataa019 <johnnymataa019@gmail.com>
2025-05-10 13:18:03 +0200

    implementing the restcontroller api

--- src/main/java/com/example/serverspringapp/controller/EmployeeController.java
+++ src/main/java/com/example/serverspringapp/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import com.example.serverspringapp.Model.Employee.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,5 +18,12 @@ public class EmployeeController {
     public List<Employee> getAllEmployee(){
     return employeeService.getAllEmployee();
 }
+  @PostMapping("/employee/")
+    public void DeteleAll(){
+    List<Employee> employees= employeeService.getAllEmployee();
+    for(Employee employee : employees){
+        return employeeService.detele(employee);
+    }
 
+  }
 }
