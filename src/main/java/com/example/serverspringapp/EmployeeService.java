a161fc96d03e0a85f21ccc63480e84ceb99332b5
mataa019 <johnnymataa019@gmail.com>
2025-05-10 11:26:31 +0200

    database connection

--- src/main/java/com/example/serverspringapp/EmployeeService.java
+++ src/main/java/com/example/serverspringapp/EmployeeService.java
@@ -4,6 +4,9 @@ import com.example.serverspringapp.Model.Employee.Employees;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class EmployeeService {
     @Autowired
@@ -12,7 +15,10 @@ public class EmployeeService {
     public void save(Employees employees){
          repository.save(employees);
     }
-    public void detel(Employees employees){
+    public List<Employees> getAllEmployee(){
+        List<Employees> employees= new ArrayList<>();
+    }
+    public void detele(Employees employees){
         repository.delete(employees);
     }
 }
