bee34f74df7868aa106fc82874eeb5ab728784dc
mataa019 <johnnymataa019@gmail.com>
2025-05-10 11:37:01 +0200

    testing add employee

--- src/test/java/com/example/serverspringapp/ServerSpringAppApplicationTests.java
+++ src/test/java/com/example/serverspringapp/ServerSpringAppApplicationTests.java
@@ -1,11 +1,19 @@
 package com.example.serverspringapp;
 
+import com.example.serverspringapp.Model.Employee.Employees;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ServerSpringAppApplicationTests {
+     @Autowired
+     private EmployeeService employeeService;
 
+     @Test
+     void addEmployee(){
+         Employees employees = new Employees();
+     }
     @Test
     void contextLoads() {
     }
